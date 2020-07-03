098f4b4aea7a2ee8d840fd1e246d382e4704a7c6
Trevor Pierce <yeahtrevorpierce@gmail.com>
2020-07-03 00:17:44 -0400

    Added small dummy login logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    const SECRET_KEY: &str = "admin";
+
+    loop {
+        let mut username = String::new();
+        let mut password = String::new();
+
+        println!("username: ");
+
+        io::stdin()
+            .read_line(&mut username)
+            .expect("Failed to read username");
+        
+        println!("password: ");
+
+        io::stdin()
+            .read_line(&mut password)
+            .expect("Failed to read password");
+        
+        let username = username.trim();
+        let password = password.trim();
+
+        if password == SECRET_KEY {
+            println!("Logged in!");
+            break;
+        }
+    }
 }
