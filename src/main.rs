f76760e5907da68a154e9f96941b4ae9be434c6f
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-02-24 21:45:22 -0500

    Updated to read configs from config file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs;
 use std::process;
 use structopt::StructOpt;
-
 use time_clock::{user::User, Cli};
 
-fn main() {
-    let args = Cli::from_args();
+#[derive(Serialize, Deserialize, Debug)]
+struct Configs {
+    connection_string: String,
+}
+
+fn main() -> std::io::Result<()> {
+    // Need to use serde to read configs for database from config.json file
+
+    let filed_configs = fs::read_to_string("config.json").expect("Could not load config.json file");
+    let configs = serde_json::to_string(&filed_configs).unwrap();
+
+    println!("{}", configs);
+
+    Ok(())
+
+    /*let args = Cli::from_args();
 
     if let Err(e) = run(args) {
         eprintln!("{}", e);
         process::exit(1);
-    }
+    }*/
 }
 
 fn run(args: Cli) -> Result<(), &'static str> {
